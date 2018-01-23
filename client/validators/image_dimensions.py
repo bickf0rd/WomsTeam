44da724ee784d1e607be243a3ab11e9344c36b39
Дмитрий Царегородцев <tsaregorodsev.d@etton.ru>
2018-01-23 13:58:29 +0300

    Исправил ошибку в миграций ValueError: Cannot serialize

--- client/validators/image_dimensions.py
+++ client/validators/image_dimensions.py
@@ -1,16 +1,21 @@
 from django.core.files.images import get_image_dimensions
 from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
 
 
-class ImageDimensions:
+@deconstructible
+class ImageDimensions(object):
     """
         Проверка изображения по высоте и ширине
     """
 
     def __init__(self, width, height):
-        self.__width, self.__height = width, height
+        self.width, self.height = width, height
 
     def __call__(self, value):
         width, height = get_image_dimensions(value)
-        if width > self.__width or height > self.__height:
-            raise ValidationError('Картинка превышает размеры {}x{}'.format(self.__width,self.__height))
+        if width > self.width or height > self.height:
+            raise ValidationError('Картинка превышает размеры {}x{}'.format(self.width, self.height))
+
+    def __eq__(self, other):
+        return other.width == self.width and other.height == self.height
